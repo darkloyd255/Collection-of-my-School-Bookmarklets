df2bfc0ef170f29b4fe2e9176fbfd85d9d59e9c8
darkloyd255 <103001982+darkloyd255@users.noreply.github.com>
2023-04-27 13:36:18 -0500

    Update Unobfuscated.js

--- messing around/Cursifier/Unobfuscated.js
+++ messing around/Cursifier/Unobfuscated.js
@@ -1,6 +1,6 @@
-let v = prompt("Text to cursify with:");
+ let v = prompt("Text to cursify with:");
     let a = prompt("Color: (MUST BE VERY DIRECT, leave blank for no change)"); 
-    for (let x = 0; x < document.getElementsByTagName("p").length; x++) {
+    for (let x = 0; x < document.querySelectorAll("p","h1","h2","h3","h4","h5").length; x++) {
       document.querySelectorAll("p","h1","h2","h3","h4","h5")[x].innerHTML = v;
         document.querySelectorAll("p","h1","h2","h3","h4","h5")[x].style.color = a;
     };
